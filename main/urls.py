f138db7fce8f94d32885bceed313e7c88f2a1a6c
unknown <krazerunited@ya.ru>
2023-02-02 18:35:06 +0300

    first commit

---
+++ main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('home', views.index, name='home'),
+    path('cart', views.cart, name='cart'),
+    path('catalog', views.categories, name='catalog'),
+    path('checkout', views.checkout, name='checkout'),
+    path('auth', views.auth, name='auth'),
+    path('registration', views.registration, name='registraion'),
+
+]
